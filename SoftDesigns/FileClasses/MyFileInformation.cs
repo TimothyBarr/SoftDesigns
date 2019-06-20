6d6e7d127d8eef575767713a8ea0a24bf17843e7
Tim Barr <TimothyEBarr@outlook.com>
2019-06-20 09:47:30 -0500

    Add project files.

---
+++ SoftDesigns/FileClasses/MyFileInformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftDesigns.MoviesClass;
+
+namespace SoftDesigns.FileClasses
+{
+    class MyFileInformation
+    {
+        public MyFileInformation()
+        {
+            GetMovieInformation();
+        }
+
+
+        private Boolean GetMovieInformation()
+        {
+            Movies myMovies = new Movies();
+
+
+            return true;
+        }
+    }
+}
---
+++ SoftDesigns/Movies/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftDesigns.MoviesClass
+{
+    class Movies
+    {
+        public Movies()
+        {
+            GetMovies();
+        }
+
+        private void GetMovies()
+        {
+            return;
+        }
+    }
+}
